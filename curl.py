360133d6a7f0b72d5bf177040ccb6f9c43376853
Sergey Shiryev <grayyyyy456@yandex.ru>
2025-03-14 19:21:19 +0300

    Генерация рандомных данных была перенесена в файл helper.py Добавлена фикстура для удаления курьера Изменены тесты test_create_two_identical_couriers, test_create_courier

--- curl.py
+++ curl.py
@@ -3,3 +3,4 @@ creating_courier_api = main_site + 'api/v1/courier'   # POST создание к
 login_courier_api = main_site + 'api/v1/courier/login'   # POST вход курьера
 creating_order_api = main_site + 'api/v1/orders'   # POST создание заказа
 list_order_api = main_site + 'api/v1/orders'   # GET получение листа заказов
+delete_courier_api = main_site + 'api/v1/courier/'  # DELETE удаление курьера
--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -1,22 +1,20 @@
 import requests
 from data import DataCourier
-from conftest import register_new_courier_and_return_login_password
 from curl import creating_courier_api
 import allure
 
 class TestCreateCourier:
     @allure.title("Проверка на создание курьера")
-    def test_create_courier(self):
-        data_create_courier = register_new_courier_and_return_login_password()
-        response = requests.post(creating_courier_api, json=data_create_courier)
-        assert response.status_code == 201 and response.text == '{"ok":true}'
+    def test_create_courier(self, courier):
+        assert courier['login'] is not None
+        assert courier['password'] is not None
+        assert courier['firstName'] is not None
 
     @allure.title("Проверка на ошибку, при попытке создать двух одинаковых курьера")
-    def test_create_two_identical_couriers(self):
-        data_create_courier = register_new_courier_and_return_login_password()
-        response_1 = requests.post(creating_courier_api, json=data_create_courier)
-        response_2 = requests.post(creating_courier_api, json=data_create_courier)
-        assert response_2.status_code == 409 and 'Этот логин уже используется. Попробуйте другой.' in response_2.text
+    def test_create_two_identical_couriers(self, courier):
+        data_create_courier = courier
+        response = requests.post(creating_courier_api, json=data_create_courier)
+        assert response.status_code == 409 and 'Этот логин уже используется. Попробуйте другой.' in response.text
 
     @allure.title("Проверка на ошибку, при создании курьера без логина")
     def test_create_courier_without_login(self):
